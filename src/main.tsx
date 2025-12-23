ffcfc7859c53bb1a2a9267102f03fbd4a98169ec
infololie-design <info.lolie@gmail.com>
2025-12-23 22:09:12 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import { registerServiceWorker } from './registerSW';
 
-registerServiceWorker();
+// --- KILL SWITCH (ÖNBELLEK TEMİZLEYİCİ) ---
+// Eğer tarayıcıda kayıtlı bir Service Worker varsa, onu bul ve yok et.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister()
+        .then(() => console.log('Eski Service Worker silindi. Site güncelleniyor...'));
+    }
+  });
+  
+  // Önbellekleri de temizlemeye çalış
+  if ('caches' in window) {
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name);
+      });
+    });
+  }
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
